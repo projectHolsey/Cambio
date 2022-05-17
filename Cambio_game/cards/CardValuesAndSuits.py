108130c5a23576b8c796a26ecf898a710251229e
MAtt <Matt@quarch.com>
2022-05-17 19:43:29 +0100

    Making into porduction instance

---
+++ Cambio_game/cards/CardValuesAndSuits.py
@@ -0,0 +1,11 @@
+value = ['ace', '2', '3', '4',
+         '5', '6', '7', '8',
+         '9', '10', 'jack', 'queen',
+         'king', 'joker']
+
+suits = ['heart', 'diamonds', 'spades', 'clubs']
+
+colours = ['red', 'black']
+
+
+
